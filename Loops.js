400e7997b075c6feb0579e6112cdf1d56bcd634b
majojukrishna03 <majojukrishna03@gmail.com>
2024-07-10 23:45:47 +0530

    added conditional statements and for loop,while loop exercise

---
+++ Loops.js
@@ -0,0 +1,104 @@
+/*
+In this exercise, you will practice writing "for" and "while" loops.
+
+Task 1
+Write a "for" loop that will perform exactly the same repetitive code as this:
+
+1234567
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!')
+
+Reset
+Task 2 
+Write a "for" loop that will perform exactly the same repetitive code as this:
+
+1234567
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+
+Reset
+Task 3
+Write a "while" loop that will perform exactly the same repetitive code as this:
+
+1234567
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!')
+
+Reset
+Note: Name your increment variable i. Update the variable in the while loop using i++.
+
+Task 4
+Write a "while" loop that will perform exactly the same repetitive code as this:
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+
+Reset
+Note: In the while loop, decrement the value of i using: i = i - 1.
+
+Task 5
+Write a "while" loop that will perform exactly the same repetitive code as this:
+
+123456
+console.log(2018)
+console.log(2019)
+console.log(2020)
+console.log(2021)
+console.log(2022)
+
+Reset
+
+
+*/
+
+// task 1
+
+// for (i = 1;i<6;i++) {
+//     console.log(i);
+// }
+// console.log("Counting completed!");
+
+// task 2
+
+// for (i=5;i>0;i--) {
+//     console.log(i);
+// } console.log("Countdown finished!");
+
+// task 3
+
+// var i = 1;
+// while (i<6) {
+//     console.log(i);
+//     i++;
+// } console.log('Counting completed!');
+
+// task 4
+
+// var i = 5;
+// while (i > 0) {
+//     console.log(i);
+//     i--;
+// } console.log("Countdown finished!");
+
+// task 5
+
+// var year = 2018;
+// while (year <2023) {
+//     console.log(year);
+//     year++;
+// } 
